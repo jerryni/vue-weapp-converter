e17159b9202ce43b8d20dc3421ea00de9684e4ea
nirizhe <nirizhe@youzan.com>
2021-07-14 16:49:28 +0800

    ut

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,6 +1,14 @@
 import { expect } from '@jest/globals';
-import { wrapVar } from '../src/weapp/helper';
+import { wrapVar, transWeappAttr } from '../src/weapp/helper';
 
-test('', () => {
+test('wrapVar', () => {
   expect(wrapVar('1')).toEqual('{{ 1 }}');
 })
+
+test('transWeappAttr', () => {
+  expect(transWeappAttr({ name: 'v-if', value: 'param' })).toEqual({
+    name: 'wx:if',
+    value: '{{ param }}',
+    extraStr: ''
+  })
+});
